d8389b56d7a7acef73f09bdc1f5e76ae13b6ab70
Yebin Lee <71310074+YebinLeee@users.noreply.github.com>
2021-02-22 00:59:17 +0900

    Delete ch16 - 전처리 및 다중소스파일/다중 소스 파일 - 헤더 파일 적용 directory

--- ch16 - 전처리 및 다중소스파일/다중 소스 파일 - 헤더 파일 적용/rect.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-typedef struct rect {
-	int x, y, w, h;
-}RECT;
-
-void draw_rect(const RECT*);
-double calc_area(const RECT*);
-void move_rect(RECT*, int, int); // 원점을 이동하게 되므로 const키워드를 붙여서는 안 된다
